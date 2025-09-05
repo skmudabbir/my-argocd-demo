73c3cf74fc37ef30078af7cf4f7e3dd5d45d77d1
Syed Mudabbir <mudabbir1187@gmail.com>
2025-09-05 12:32:39 -0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,1 +1,1 @@
-const http = require('http');http.createServer((_, res) => res.end('Hello from ArgoCD')).listen(3000);
+const http = require('http');http.createServer((_, res) => res.end('Hello from ArgoCD')).listen(4000);
